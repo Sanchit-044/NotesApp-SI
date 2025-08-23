5a637e8f4b318927ec11a0e4c185ff56d0bd541c
Sanchit-044 <sanchitbhardwaj044@gmail.com>
2025-08-23 22:17:51 +0530

    Update PublicNotes.jsx

--- frontend/src/pages/PublicNotes.jsx
+++ frontend/src/pages/PublicNotes.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
 
-const API = import.meta.env.VITE_API_URL || "http://localhost:5000";
+const API = import.meta.env.VITE_API_URL || "https://notesapp-si-backend.onrender.com";
 
 export default function PublicNotes() {
   const [notes, setNotes] = useState([]);
